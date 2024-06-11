6549313d30d92aa33c38c501df86d98a9f3e98b2
Rithu Kaimal <rithukaimal@gmail.com>
2024-06-11 09:49:03 -0500

    Initial Commit

---
+++ ecotiqueProject/ecotiqueProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ecotiqueProjectApp.swift
+//  ecotiqueProject
+//
+//  Created by Scholar on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ecotiqueProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
